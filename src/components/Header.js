0ee794af13ea6516e01a3e1556cdb50bf2c8ff46
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2023-03-20 21:42:45 -0300

    change state

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import './Header.scss';
 import { HashLink as Link } from 'react-router-hash-link';
 
 export default function Header() {
-  const display = () =>{
-    
-  }
+  const [visible, setVisible] = useState(false)
 
   return (
     <div className='header-container row align-items-center'>
@@ -13,10 +11,10 @@ export default function Header() {
         <a href="/"><h1>Idear.</h1></a>
       </div>
       <div className='menu-burger col-6'>
-        <i className="bi bi-list" onClick={()=>setDisplay(!display)}></i>
+        <i className="bi bi-list" onClick={()=> setVisible(!visible)}></i>
       </div>
-      {display &&
-        <div className='col-12 col-sm-6 nav-content'>
+      {visible &&
+        <div className='col-12 col-sm-6 responsive-content'>
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/#quienes-somos">Quienes Somos</Link></li>
@@ -26,6 +24,15 @@ export default function Header() {
           </ul>
         </div>
       }
+      <div className='col-12 col-sm-6 nav-content'>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/#quienes-somos">Quienes Somos</Link></li>
+          <li><Link to="/objetivos">Objetivos</Link></li>
+          <li><Link to="/#contacto">Contacto</Link></li>
+          <li><Link to="/#dependencias">Dependencias</Link></li>
+        </ul>
+      </div>
       <div className='col-4 button-container'>
         <button><a href="/#contacto">Quiero Sumar</a></button>
       </div>
